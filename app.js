f9ba5bfa36bc5bd7e092476164b99f2bfa5f62cb
Randeep Singh <randeeprattan90@gmail.com>
2020-05-28 15:58:27 +0100

    Setup markup and styling

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("app js");
+const rulesBtn = document.getElementById("rules-btn"),
+  closeBtn = document.getElementById("close-btn"),
+  rules = document.getElementById("rules");
+
+// Rules and close event handlers
+rulesBtn.addEventListener("click", () => rules.classList.add("show"));
+closeBtn.addEventListener("click", () => rules.classList.remove("show"));
