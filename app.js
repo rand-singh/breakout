fbf0351c75f09e134290968c158a5ada156b608b
Randeep Singh <randeeprattan90@gmail.com>
2020-05-28 17:50:36 +0100

    Update app.js
    Create the bricks

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const rulesBtn = document.getElementById("rules-btn"),
   closeBtn = document.getElementById("close-btn"),
   rules = document.getElementById("rules"),
   canvas = document.getElementById("canvas"),
-  ctx = canvas.getContext("2d");
+  ctx = canvas.getContext("2d"),
+  brickRowCount = 9,
+  brickColumnCount = 5;
 
 let score = 0;
 
@@ -26,6 +28,32 @@ const paddle = {
   dx: 0,
 };
 
+// Create brick props
+const brickInfo = {
+  w: 70,
+  h: 20,
+  padding: 10,
+  offsetX: 45,
+  offsetY: 60,
+  visible: true,
+};
+
+// Create bricks
+const bricks = [];
+for (let i = 0; i < brickRowCount; i++) {
+  bricks[i] = [];
+  for (let j = 0; j < brickColumnCount; j++) {
+    const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
+    const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
+    bricks[i][j] = {
+      x,
+      y,
+      ...brickInfo,
+    };
+  }
+}
+console.log(bricks);
+
 // Draw ball on the canvas
 function drawBall() {
   ctx.beginPath();
@@ -45,17 +73,32 @@ function drawPaddle() {
   ctx.closePath();
 }
 
+// Draw score on canvas
+function drawScore() {
+  (ctx.font = "20px Arial"),
+    ctx.fillText(`Score: ${score}`, canvas.width - 100, 35);
+}
+
+// Draw bricks on canvas
+function drawBricks() {
+  bricks.forEach(column => {
+    column.forEach(brick => {
+      ctx.beginPath();
+      ctx.rect(brick.x, brick.y, brick.w, brick.h);
+      ctx.fillStyle = brick.visible ? "#0095dd" : "transparent";
+      ctx.fill();
+
+      ctx.closePath();
+    });
+  });
+}
+
 // Draw game items
 function draw() {
   drawBall();
   drawPaddle();
   drawScore();
-}
-
-// Draw score on canvas
-function drawScore() {
-  (ctx.font = "20px Arial"),
-    ctx.fillText(`Score: ${score}`, canvas.width - 100, 35);
+  drawBricks();
 }
 
 draw();
